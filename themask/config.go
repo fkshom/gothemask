4e667edc820a3e16dce8ad0fe09be8c549cac611
Shoma FUKUDA <fkshom@gmail.com>
2021-06-23 19:18:17 +0900

    refact

--- themask/config.go
+++ themask/config.go
@@ -22,18 +22,6 @@ type Engineconfig struct {
 	Engine string
 }
 
-func merge(m1, m2 map[string]string) map[string]string {
-	ans := map[string]string{}
-
-	for k, v := range m1 {
-		ans[k] = v
-	}
-	for k, v := range m2 {
-		ans[k] = v
-	}
-	return (ans)
-}
-
 func MapToStruct(m map[string]interface{}, val interface{}) error {
 	tmp, err := jsoniter.Marshal(m)
 	if err != nil {
